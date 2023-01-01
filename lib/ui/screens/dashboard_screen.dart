e3b1dacbfa7366501e97e2e7c5fc85359f5576f5
DevikaRajesh22 <devikaraj699@gmail.com>
2023-01-01 12:57:00 +0530

    Side bar texts and icons aligned

--- lib/ui/screens/dashboard_screen.dart
+++ lib/ui/screens/dashboard_screen.dart
@@ -39,156 +39,177 @@ class DashboardScreen extends StatelessWidget {
                   const SizedBox(
                     height: 60,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Icon(
-                          Icons.dashboard_sharp,
-                          color: Color.fromARGB(255, 255, 255, 255),
+                  SizedBox(
+                    width: 140,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.dashboard_sharp,
+                            color: Color.fromARGB(255, 255, 255, 255),
+                          ),
                         ),
-                      ),
-                      Text(
-                        'Dashboard',
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 20,
+                        Text(
+                          'Dashboard',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontSize: 20,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                   const SizedBox(
-                    height: 10,
+                    height: 15,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Icon(
-                          Icons.account_circle_rounded,
-                          color: Color.fromARGB(255, 255, 255, 255),
+                  SizedBox(
+                    width: 140,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.account_circle_rounded,
+                            color: Color.fromARGB(255, 255, 255, 255),
+                          ),
                         ),
-                      ),
-                      Text(
-                        'Profile',
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 20,
+                        Text(
+                          'Profile',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontSize: 20,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Icon(
-                          Icons.location_on,
-                          color: Color.fromARGB(255, 255, 255, 255),
-                        ),
-                      ),
-                      Text(
-                        'Map',
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 20,
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                   const SizedBox(
-                    height: 10,
+                    height: 15,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Icon(
-                          Icons.query_stats,
-                          color: Color.fromARGB(255, 255, 255, 255),
+                  SizedBox(
+                    width: 140,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.location_on,
+                            color: Color.fromARGB(255, 255, 255, 255),
+                          ),
                         ),
-                      ),
-                      Text(
-                        'Statistics',
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 20,
+                        Text(
+                          'Map',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontSize: 20,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Icon(
-                          Icons.library_books_rounded,
-                          color: Color.fromARGB(255, 255, 255, 255),
-                        ),
-                      ),
-                      Text(
-                        'Class',
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 20,
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                   const SizedBox(
-                    height: 10,
+                    height: 15,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Icon(
-                          Icons.notifications,
-                          color: Color.fromARGB(255, 255, 255, 255),
+                  SizedBox(
+                    width: 140,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.query_stats,
+                            color: Color.fromARGB(255, 255, 255, 255),
+                          ),
                         ),
-                      ),
-                      Text(
-                        'Notification',
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 20,
+                        Text(
+                          'Statistics',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontSize: 20,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  SizedBox(
+                    width: 140,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.library_books_rounded,
+                            color: Color.fromARGB(255, 255, 255, 255),
+                          ),
+                        ),
+                        Text(
+                          'Class',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  SizedBox(
+                    width: 150,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.notifications,
+                            color: Color.fromARGB(255, 255, 255, 255),
+                          ),
+                        ),
+                        Text(
+                          'Notification',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                   const SizedBox(
                     height: 250,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Icon(
-                          Icons.logout_rounded,
-                          color: Color.fromARGB(255, 255, 255, 255),
+                  SizedBox(
+                    width: 140,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.logout_rounded,
+                            color: Color.fromARGB(255, 255, 255, 255),
+                          ),
                         ),
-                      ),
-                      Text(
-                        'Logout',
-                        style: TextStyle(
-                          color: Color.fromARGB(220, 255, 255, 255),
-                          fontSize: 15,
+                        Text(
+                          'Logout',
+                          style: TextStyle(
+                            color: Color.fromARGB(220, 255, 255, 255),
+                            fontSize: 15,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ],
               ),
@@ -199,106 +220,8 @@ class DashboardScreen extends StatelessWidget {
                   width: 700,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      const SizedBox(
-                        height: 100,
-                      ),
-                      const Text(
-                        'CREATE YOUR FREE ACCOUNT',
-                        style: TextStyle(
-                          fontSize: 29,
-                          fontWeight: FontWeight.w600,
-                          color: Color.fromARGB(255, 62, 111, 206),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 70,
-                      ),
-                      const TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Organisation name',
-                          hintText: 'Full name',
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      const TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Email address',
-                          hintText: 'user@gmail.com',
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      const TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Password',
-                          suffixIcon: Icon(
-                            Icons.visibility,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 40,
-                      ),
-                      SizedBox(
-                        width: 400,
-                        child: Material(
-                          elevation: 4,
-                          color: const Color.fromARGB(255, 62, 111, 206),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            side: const BorderSide(
-                              color: Color.fromARGB(255, 5, 27, 70),
-                              width: 0.0001,
-                            ),
-                          ),
-                          textStyle: const TextStyle(
-                              color: Color.fromARGB(255, 255, 255, 255)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(10),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: const [
-                                Text('Create your account'),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      const Text(
-                        'OR',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      Row(
-                        children: const [
-                          Text(
-                            'By sigining up, you agree to our communication and usage terms. Already have an account?',
-                          ),
-                          Text(
-                            ' Sign in',
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 29, 100, 244),
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ],
-                      ),
+                    children: const [
+                      //type column children here
                     ],
                   ),
                 ),

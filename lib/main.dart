7d81133cc10b45d96f19a6927a8d1b3ea9dae685
DevikaRajesh22 <devikaraj699@gmail.com>
2023-01-02 10:36:33 +0530

    map screen side bar created

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:refuge_ngo/ui/screens/profilesec_screen.dart';
+import 'package:refuge_ngo/ui/screens/map_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ProfilesecScreen(),
+      home: const MapScreen(),
     );
   }
 }

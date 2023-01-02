362a23f6a4fd7fbc9d67fcc7fdd9533f812390a8
DevikaRajesh22 <devikaraj699@gmail.com>
2023-01-02 10:55:15 +0530

    notification screen side bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:refuge_ngo/ui/screens/map_screen.dart';
+import 'package:refuge_ngo/ui/screens/notification_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MapScreen(),
+      home: const NotificationScreen(),
     );
   }
 }

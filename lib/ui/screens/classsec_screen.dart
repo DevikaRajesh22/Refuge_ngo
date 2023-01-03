51acd08e1dcc5032fd09f187a74de1b186a75887
DevikaRajesh22 <devikaraj699@gmail.com>
2023-01-03 21:48:57 +0530

    Second class screen completed

--- lib/ui/screens/classsec_screen.dart
+++ lib/ui/screens/classsec_screen.dart
@@ -260,9 +260,93 @@ class ClasssecScreen extends StatelessWidget {
                         ],
                       ),
                       const SizedBox(
+                        height: 50,
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Speaker name',
+                          hintText: 'Full name',
+                        ),
+                      ),
+                      const SizedBox(
                         height: 10,
                       ),
-
+                      const TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Topic',
+                          hintText: 'Topic by speaker',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Location',
+                          hintText: 'Camp number',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Date',
+                          hintText: 'dd-mm-yyyy',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Start time',
+                          hintText: 'hh:mm am/pm',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'End time',
+                          hintText: 'hh:mm am/pm',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      SizedBox(
+                        width: 300,
+                        child: Material(
+                          elevation: 4,
+                          color: const Color.fromARGB(255, 84, 133, 234),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            side: const BorderSide(
+                              color: Color.fromARGB(255, 5, 27, 70),
+                              width: 0.0001,
+                            ),
+                          ),
+                          textStyle: const TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Text('Save'),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
                       //column items
                     ],
                   ),
